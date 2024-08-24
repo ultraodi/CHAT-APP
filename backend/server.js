fe6640d93eae753537d942925b9cc0164644de18
ultraodi <odinamclint@gmail.com>
2024-08-24 16:06:46 -0400

    Establishing the mongoose connection and creating a signup function

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js"
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming request with JSON payloads (form req.body)
+
+// this is a middleware
+app.use("/api/auth", authRoutes)
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!")
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB()
+  console.log(`Server running on Port ${PORT}`)
+});
